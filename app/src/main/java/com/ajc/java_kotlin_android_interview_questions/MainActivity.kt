6641e8c6c9feb17f7cf80bf90be652ea9c2e2e61
Avadhutjc <Avadhutjc@gmail.com>
2022-02-28 23:02:07 +0530

    first

---
+++ app/src/main/java/com/ajc/java_kotlin_android_interview_questions/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ajc.java_kotlin_android_interview_questions
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
